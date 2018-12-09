4611aa1b200898e9f6d41c97656f5f2e7027fdeb
august von trapp <augustvontrapp@gmail.com>
2018-12-08 23:36:12 -0800

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import * as p5 from 'p5';
+
+let s = (sk) => {
+
+  sk.setup = () => {
+    sk.createCanvas(800, 800);
+    sk.background(230);
+    sk.strokeWeight(2);
+  }
+
+  sk.draw = () => {
+    if (sk.mouseIsPressed){
+      sk.stroke(255);
+    }
+    else {
+      sk.stroke(237,34,93);
+    }
+    sk.line(sk.mouseX-66, sk.mouseY, sk.mouseX+66, sk.mouseY);
+    sk.line(sk.mouseX, sk.mouseY-66, sk.mouseX, sk.mouseY+66);
+  }
+}
+
+const P5 = new p5(s);
